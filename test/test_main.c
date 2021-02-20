ea3bdd0f448dcef6e2c21b0b34fc65fe5adb31fd
Matteo Zini <matteozini96@gmail.com>
2021-02-20 19:52:41 +0100

    Test polishing

--- test/test_main.c
+++ test/test_main.c
@@ -7,8 +7,9 @@
 #include <sys/syscall.h>
 #include <stdlib.h>
 
-#define NUM_THREADS		2
-//#define NUM_SLOTS 		5
+#define NUM_THREADS		5
+#define NUM_SLICES 		8
+#define SLICE_SIZE		1500  //milliseconds
 
 #define gettid() syscall(SYS_gettid)
 
@@ -61,10 +62,13 @@ int main()
 	char buf[100];
 	int ret, i;
 	pthread_t mythread[NUM_THREADS];
+	char mess_buff[50];
 
 	int fd = open("/dev/scheduler_setting", O_RDWR);
 
-	write(fd, "num_slices:8;slice_size:1500", 29);
+	sprintf(mess_buff, "num_slices:%u;slice_size:%u", NUM_SLICES, SLICE_SIZE);
+
+	write(fd, mess_buff, strlen(mess_buff)+1);
 	read(fd, buf, 100);
 
 	printf("%s\n", buf);
@@ -72,9 +76,9 @@ int main()
 	//create the threads (as an argument I communicate the scehduling slot, since only the thread itself knows its tid)
 	ret = pthread_create(&mythread[0], NULL , thread_body_common, (void*)2);
 	ret |= pthread_create(&mythread[1], NULL , thread_body_common, (void*)1);
-	ret |= pthread_create(&mythread[1], NULL , thread_body_common, (void*)4);
-	ret |= pthread_create(&mythread[1], NULL , thread_body_common, (void*)5);
-	ret |= pthread_create(&mythread[1], NULL , thread_body_common, (void*)6);
+	ret |= pthread_create(&mythread[2], NULL , thread_body_common, (void*)4);
+	ret |= pthread_create(&mythread[3], NULL , thread_body_common, (void*)5);
+	ret |= pthread_create(&mythread[4], NULL , thread_body_common, (void*)6);
 
 	if (ret) {
 		printf("Errore nella creazione dei thread\n");
